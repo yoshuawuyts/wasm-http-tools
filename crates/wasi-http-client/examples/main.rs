a1f5b551b29692f83bfb6cf42bf3c4f8072044a6
Yosh <github@yosh.is>
2024-02-26 17:29:33 +0100

    add a run command

---
+++ crates/wasi-http-client/examples/main.rs
@@ -0,0 +1,19 @@
+use wasi::http::{
+    outgoing_handler::{handle, OutgoingRequest},
+    types::{Fields, Method, RequestOptions},
+};
+use wasi_http_client::Poller;
+
+fn main() {
+    let mut poller = Poller::new();
+    let fields = Fields::new();
+    let req = OutgoingRequest::new(fields);
+    req.set_method(&Method::Get).unwrap();
+    req.set_path_with_query(Some("http://example.com")).unwrap();
+    let res = handle(req, None).unwrap();
+    let pollable = res.subscribe();
+    poller.insert(pollable);
+    // assert!(&pollable.ready(), "pollable was not ready");
+    poller.block_until();
+    dbg!(res);
+}
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!bash
+set pipefail -euo
+
+cargo build --target wasm32-wasi --example main
+wasm-tools component new target/wasm32-wasi/debug/examples/main.wasm \
+    --adapt resources/wasi_snapshot_preview1.command.wasm \
+    -o target/main.wasm
+wasmtime run -S http target/main.wasm
