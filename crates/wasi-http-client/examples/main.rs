cc07a4352ad99cca6f846d3ca7732c26d10c33e8
Yosh <github@yosh.is>
2024-02-29 04:23:02 +0100

    clean up the little http client example

--- crates/wasi-http-client/examples/main.rs
+++ crates/wasi-http-client/examples/main.rs
@@ -1,19 +1,25 @@
 use wasi_http_client::runtime::block_on;
-use wasi_http_client::{Client, Method, Request};
+use wasi_http_client::{Client, Method, Request, Response};
 
 fn main() {
     block_on(|reactor| async {
         let client = Client::new(reactor);
+
         let url = "https://example.com".parse().unwrap();
         let req = Request::new(Method::Get, url);
-        let mut res = client.send(req).await;
+        let res = client.send(req).await;
 
-        let mut output = vec![];
-        while let Some(chunk) = res.next_chunk().await {
-            let chunk = chunk.unwrap();
-            output.extend_from_slice(&chunk);
-        }
-
-        dbg!(String::from_utf8(output).unwrap());
+        let body = read_to_end(res).await;
+        let body = String::from_utf8(body).unwrap();
+        println!("{body}");
     })
+}
+
+async fn read_to_end(mut res: Response) -> Vec<u8> {
+    let mut body = vec![];
+    while let Some(chunk) = res.next_chunk().await {
+        let chunk = chunk.unwrap();
+        body.extend_from_slice(&chunk);
+    }
+    body
 }
